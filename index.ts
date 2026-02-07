8e6f0d8da6f6e53fd08beb315d63a5472ae7add8
Jhoss2 <Jhoss8631@gmail.com>
2026-02-07 14:10:04 +0100

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,20 @@
+export const Colors = {
+  primary: '#b91c1c', // red-800
+  secondary: '#1e3a8a', // blue-900
+  background: '#fde7f3',
+  white: '#ffffff',
+  gray: '#e5e7eb',
+  text: '#1f2937',
+  error: '#ef4444',
+  success: '#10b981',
+  adminRed: '#7f1d1d', // red-900
+};
+
+export const Fonts = {
+  inter: 'Inter-Regular',
+  interBold: 'Inter-Bold',
+  interSemiBold: 'Inter-SemiBold',
+  interMedium: 'Inter-Medium',
+  algerian: 'Algerian',
+  monotypeCorsiva: 'MonotypeCorsiva',
+};
